ae83f525547ef050f9a4d4fd9bc0ef20421bff1b
xujinchang <xjc1@bupt.edu.cn>
2017-12-20 16:33:15 +0800

    add AUEGAN

--- config.py
+++ config.py
@@ -1,13 +1,24 @@
 from models import *
 
 
-model_zoo = ['WGAN-GP']
+model_zoo = ['DCGAN', 'LSGAN', 'WGAN', 'WGAN-GP', 'EBGAN', 'BEGAN', 'DRAGAN']
 
 def get_model(mtype, name, training):
     model = None
-    if mtype == 'WGAN-GP':
+    if mtype == 'DCGAN':
+        model = dcgan.DCGAN
+    elif mtype == 'LSGAN':
+        model = lsgan.LSGAN
+    elif mtype == 'WGAN':
+        model = wgan.WGAN
+    elif mtype == 'WGAN-GP':
         model = wgan_gp.WGAN_GP
-
+    elif mtype == 'EBGAN':
+        model = ebgan.EBGAN
+    elif mtype == 'BEGAN':
+        model = began.BEGAN
+    elif mtype == 'DRAGAN':
+        model = dragan.DRAGAN
     else:
         assert False, mtype + ' is not in the model zoo'
 
@@ -18,16 +29,12 @@ def get_model(mtype, name, training):
 
 def get_dataset(dataset_name):
     # celebA_64 = './data/celebA_tfrecords/*.tfrecord'
-    # celebA_64 = '/home/xujinchang/GAN_lanc/lan_tfrecords_8_64/*.tfrecord'
     celebA_64 = '/home/xujinchang/GAN_celeA_LR_HR/celebA_tfrecords_8_64/*.tfrecord'
     lsun_bedroom_128 = './data/lsun/bedroom_128_tfrecords/*.tfrecord'
 
     if dataset_name == 'celeba':
         path = celebA_64
         n_examples = 202599
-    # if dataset_name == 'celeba':
-    #    path = celebA_64
-    #    n_examples = 40000
     elif dataset_name == 'lsun':
         path = lsun_bedroom_128
         n_examples = 3033042
